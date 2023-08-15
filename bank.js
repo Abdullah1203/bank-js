b810b8e8798fea211e92cfa04ae7b73bb076a926
Abdullah Ibne Ali <abdullah.ibneali152pro@gmail.com>
2023-08-16 01:17:55 +0600

    all fixed

--- bank.js
+++ bank.js
@@ -1,6 +1,6 @@
 let totalDepositAmount = 0;
 let totalWithdrawAmount = 0;
-let totalBalanceAmount = 21000;
+let totalBalanceAmount = 20500;
 
 // DEPOSIT js
 document
@@ -15,15 +15,50 @@ document
     const depositTotal = document.getElementById("deposit");
     const balanceTotal = document.getElementById("balance");
     const deposit = depositTotal.innerText;
-
-    //calculating deposit + total balance 
-    totalDepositAmount = parseInt(depositAmount) + totalDepositAmount;
-    totalBalanceAmount = totalBalanceAmount + parseInt(depositAmount);
-    // updating total deposit & total balance
-    depositTotal.innerText = totalDepositAmount;
-    balanceTotal.innerText = totalBalanceAmount;
+    if (depositFeild.value == "") {
+      alert("please Enter an amount to DEPOSITE 💵");
+    } else {
+      //calculating deposit + total balance
+      totalDepositAmount = parseFloat(depositAmount) + totalDepositAmount;
+      totalBalanceAmount = totalBalanceAmount + parseFloat(depositAmount);
+      
+      // updating total deposit & total balance
+      depositTotal.innerText = totalDepositAmount;
+      balanceTotal.innerText = totalBalanceAmount;
+      depositFeild.value = '';
+    
+    }
   });
 
+// WITHDRAW JS
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    const withdrawFeild = document.getElementById("withdraw-amount");
+    const withdrawAmount = withdrawFeild.value;
 
-  // WITHDRAW JS
-  
+    console.log(withdrawAmount);
+
+    //withdraw total amount
+    const withdrawTotal = document.getElementById("withdraw");
+    const balanceTotal = document.getElementById("balance");
+    const withdraw = withdrawTotal.innerText;
+
+    //calculating withdraw + total balance
+    if (withdrawFeild.value == "") {
+      alert("please Enter an amount to Withdraw 💸");
+    } else {
+      totalWithdrawAmount = parseFloat(withdrawAmount) + totalWithdrawAmount;
+      
+      if(totalBalanceAmount < parseFloat(withdrawAmount)){
+        alert("Balance Insufficient");
+        withdrawFeild.value = '';
+      }else{
+      // updating total withdraw & total balance
+      totalBalanceAmount = totalBalanceAmount - parseFloat(withdrawAmount);
+      withdrawTotal.innerText = totalWithdrawAmount;
+      balanceTotal.innerText = totalBalanceAmount;
+      withdrawFeild.value = '';
+      }
+    }
+  });
