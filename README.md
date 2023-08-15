eafc8e829c80a99bf6df648649f06d329f3c874b
Abdullah Ibne Ali <abdullah.ibneali152pro@gmail.com>
2023-08-15 23:16:13 +0600

    app.js + bank.html added and designed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # "bank-js" is a Simple BANK website
 ### It lets you deposit and withdraw money from your account
 
-Live server link :  [Bank JS 💸](https://abdullah1203.github.io/bank-js)
+Live server link :  [Bank JS 💸](https://abdullah1203.github.io/bank-js/)
--- app.js
+++ app.js
@@ -1,1 +1,17 @@
- 
+// step 1 : adding submit button with event handler
+
+document.getElementById("submit").addEventListener("click", function () {
+  // step 2 : adding input field get value
+  // always use .value to get value
+
+  const email = document.getElementById("email");
+
+  //step 3 : get password
+  const password = document.getElementById("password");
+
+  if (email.value === "admin" && password.value === "admin") {
+    location.href = "http://127.0.0.1:5500/BANK%20PROJECT%20JS/bank.html";
+  } else {
+    alert("WRONG INFO 😡");
+  }
+});
